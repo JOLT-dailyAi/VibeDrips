c594a493999bf0cad4565b322a0ec98cde8611de
GitHub Action <action@github.com>
2025-12-20 15:58:16 +0000

    🔄 Process CSV data + auto-bump versions - Sat Dec 20 15:58:16 UTC 2025

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - Service worker for VibeDrips PWA
 // CACHE_VERSION is now timestamp-based and auto-updated by the workflow
 
-const CACHE_VERSION = 'v20251218T161733'; // This string will be overwritten by process-csv.yml
+const CACHE_VERSION = 'v20251220T155816'; // This string will be overwritten by process-csv.yml
 const CACHE_NAME = `vibedrips-static-${CACHE_VERSION}`;
 const DATA_CACHE = `vibedrips-data-${CACHE_VERSION}`;
 
