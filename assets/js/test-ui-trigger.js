2bcdd6012dbeef004b0e44158d8abb9fb05044a1
Jack of all Traits <jack.of.all.traits.official@gmail.com>
2026-02-10 03:20:57 +0530

    test: cleanup test trigger file [skip ci]

--- assets/js/test-ui-trigger.js
+++
@@ -1,3 +0,0 @@
-// This is a test file to trigger the PWA UI versioning workflow.
-// Creating this file should result in a CACHE_VERSION bump with the -ui suffix in sw.js.
-console.log('PWA UI Update Test Triggered');
